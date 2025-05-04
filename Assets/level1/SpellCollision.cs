07397680765fac5d1030e833db292d3ff8ff5323
66110037 <66110037@kmitl.ac.th>
2025-05-05 01:38:23 +0700

    ScoreManager

--- Assets/level1/SpellCollision.cs
+++ Assets/level1/SpellCollision.cs
@@ -6,18 +6,13 @@ public class SpellCollision : MonoBehaviour
     {
         if (other.CompareTag("Monster"))
         {
-            // 1. ดึงข้อมูล Monster.cs
             Monster monster = other.GetComponent<Monster>();
             if (monster != null)
             {
-                // 2. เพิ่มคะแนน
                 ScoreManager.instance.AddScore(monster.GetScoreValue());
-
-                // 3. แจ้ง GameManager ว่ากำจัดไปแล้ว 1 ตัว
                 GameManager.instance.MonsterKilled();
             }
 
-            // 4. ทำลายมอนสเตอร์และลูกเวท
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
